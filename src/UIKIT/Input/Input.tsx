9c5e1104afb2532517ffc22469359d5276e4c56e
mariam-davitadze <mariam.davitadze.1@btu.edu.ge>
2024-02-25 21:53:00 +0400

    UPDATE: updated Input element

--- src/UIKIT/Input/Input.tsx
+++ src/UIKIT/Input/Input.tsx
@@ -1,17 +1,40 @@
-import { useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import "./Input.css";
 interface InputProps {
   type: string;
   placeholder?: string;
+  isInvalid?: boolean;
+  defaultValue?: string;
+  onChange?: (value: string) => null;
+  disabled?: boolean;
 }
-const Input = ({ type, placeholder }: InputProps) => {
+const Input = ({
+  type,
+  placeholder,
+  disabled,
+  defaultValue,
+  onChange,
+}: InputProps) => {
   const [value, setValue] = useState("");
+  useEffect(() => {
+    if (defaultValue) setValue(defaultValue);
+  }, [defaultValue]);
+  const handleValueChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+    if (onChange) {
+      onChange(e.target.value);
+    }
+  };
   return (
     <div className="custom-input">
       <input
-        className={value && "input-with-value"}
+        className={`${value && "input-with-value"} ${
+          disabled && "disabled-input"
+        }`}
         type={type}
-        onChange={(e) => setValue(e.target.value)}
+        onChange={handleValueChange}
+        disabled={disabled}
+        defaultValue={value}
       />
       <span className="input-label">{placeholder}</span>
     </div>
